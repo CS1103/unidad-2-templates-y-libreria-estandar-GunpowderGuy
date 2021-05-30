909089aadfe2a511a54f0beb474044f8f2f28e85
GunpowderGuy <diegorosario2013@gmail.com>
2021-05-30 08:33:10 -0500

    implementando primer ejercicio con ranges

--- exercises.h
+++ exercises.h
@@ -10,11 +10,27 @@
 #include <algorithm>
 
 #include <ranges>
+#include <vector>
 
+using namespace std;
+
+/*
 template<typename... Args>
 void unpack(Args.. args){
 //(v.push_back(std::forward<Args>(args)), ...);
 (args = )
+}
+*/
+
+template<typename Cont>
+auto split_range(Cont cont, int n){
+    std::vector<Cont> v;
+
+    for(auto i : ranges::iota_view(0,n+1)){
+        auto dummy = ranges::take_view(ranges::drop_view(cont,i*n),n);
+    }
+    
+    return v;
 }
 
 #endif //POO2_UNIT2_WEEK_7_2020_2_EXERCISES_H
